b8de612bfa4734b70ef714607e129fc3507503d2
api-librarian <development@messente.com>
2025-08-05 08:19:10 +0000

    Travis update: Aug 2025 (Build 953)
    [skip ci]

--- src/model/BulkOmniMessageCreateSuccessResponse.js
+++ src/model/BulkOmniMessageCreateSuccessResponse.js
@@ -1,6 +1,6 @@
 /**
  * Messente API
- * [Messente](https://messente.com) is a global provider of messaging and user verification services.  * Send and receive SMS, Viber and WhatsApp messages. * Manage contacts and groups. * Fetch detailed info about phone numbers. * Blacklist phone numbers to make sure you're not sending any unwanted messages.  Messente builds [tools](https://messente.com/documentation) to help organizations connect their services to people anywhere in the world.
+ * [Messente](https://messente.com) is a global provider of messaging and user verification services.  * Send and receive SMS, Viber, WhatsApp and Telegram messages. * Manage contacts and groups. * Fetch detailed info about phone numbers. * Blacklist phone numbers to make sure you're not sending any unwanted messages.  Messente builds [tools](https://messente.com/documentation) to help organizations connect their services to people anywhere in the world.
  *
  * OpenAPI spec version: 2.0.0
  * Contact: messente@messente.com
@@ -36,7 +36,7 @@
   /**
    * The BulkOmniMessageCreateSuccessResponse model module.
    * @module model/BulkOmniMessageCreateSuccessResponse
-   * @version 2.4.2
+   * @version 2.4.0
    */
 
   /**
